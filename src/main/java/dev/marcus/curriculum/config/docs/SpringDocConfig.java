99bc2a7888ae51dc75922c04efd6cb4f70f0238f
Marcus Linhares <marcusviniciuslinhares543@gmail.com>
2025-04-17 12:07:12 -0300

    feat: adiciona filtro e personaliza novo server request para a spring doc

--- src/main/java/dev/marcus/curriculum/config/docs/SpringDocConfig.java
+++ src/main/java/dev/marcus/curriculum/config/docs/SpringDocConfig.java
@@ -28,6 +28,9 @@ public class SpringDocConfig {
     @Value("${server.port}")
     private String serverPort;
 
+    @Value("${production.url}")
+    private String productionUrl;
+
     @Bean
     OpenAPI customOpenAPI() throws IOException {
     var key = "default";
@@ -80,7 +83,8 @@ public class SpringDocConfig {
     );
 
     var servers = new ArrayList<Server>();
-    servers.add(new Server().url("http://localhost:"+ serverPort +"/api").description("Localhost"));
+    servers.add(new Server().url("http://localhost:"+ serverPort +"/api").description("DESENVOLVIMENTO LOCALHOST"));
+    servers.add(new Server().url(this.productionUrl +"/api").description("PRODUÇÂO"));
 
     return new OpenAPI()
         .info(new Info()
