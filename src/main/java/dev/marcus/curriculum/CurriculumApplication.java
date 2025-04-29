c693c320a31cef1b514848478f39c048328fb521
Marcus Linhares <marcusviniciuslinhares543@gmail.com>
2025-04-29 09:05:53 -0300

    Feat: adiciona health check route

--- src/main/java/dev/marcus/curriculum/CurriculumApplication.java
+++ src/main/java/dev/marcus/curriculum/CurriculumApplication.java
@@ -2,12 +2,20 @@ package dev.marcus.curriculum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootApplication
+@RestController
 public class CurriculumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurriculumApplication.class, args);
 	}
 
+	@GetMapping("/ping")
+	public ResponseEntity<String> ping() {
+		return ResponseEntity.ok("pong");
+	}
 }
